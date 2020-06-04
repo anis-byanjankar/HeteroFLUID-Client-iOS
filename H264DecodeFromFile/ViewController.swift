c689042839944df31e22e24e21b4042fb90a5e50
Anish Byanjankar <anis.byanjankar@gmail.com>
2020-06-04 21:38:17 +0900

    Cleanup of the code.

--- H264DecodeFromFile/ViewController.swift
+++ H264DecodeFromFile/ViewController.swift
@@ -118,59 +118,9 @@ class ViewController: UIViewController,RTPPacketDelegate,VideoDecoderDelegate {
         
         configDecoding(fromFile: false)
         
-        //******************************TESTING*********************************
-        //testUDPServer()
-        //testSeparatePacket()
-        //              fxnA() // Dispatch Thread testing.
-        
-        //        sleep(2)
-        //        self.receivedIndividualNalFrame(&SPS)
-        //        self.receivedIndividualNalFrame(&PPS)
         
     }
     
-    //Pass by reference checking.
-    func fxnA(){
-        var a: UInt8! = 0x87
-        var b: UInt8! = 0x80
-        DispatchQueue.global(qos: .userInteractive).async {
-            sleep(3)
-            self.fxnB(&a)
-        }
-        
-        DispatchQueue.global(qos: .userInteractive).async {
-            sleep(1)
-            self.fxnB(&b)
-        }
-    }
-    
-    func fxnB(_ data: inout UInt8){
-        for _ in 1...50{
-            print(data)
-            sleep(1)
-        }
-    }
-    //Pass by reference Checking end.
-    func testSeparatePacket(){
-        var dummy: [UInt8] = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x01,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x01,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFA,0x1B]
-        var dummy2: [UInt8] = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFA,0x1B]
-        self.rtpParser?.separatePackets(&dummy);
-        self.rtpParser?.separatePackets(&dummy2);
-        
-    }
-    
-    func testUDPServer(){
-        
-        
-        //        DispatchQueue.global(qos: .background).async {
-        //            let x = try! UDPServer(port: 9876)//HardCoded in the AOSP ARTPWriter
-        //
-        //            for _ in 1...2{
-        //                sleep(1)
-        //                print(String(decoding: x.Receive() ?? Data("NO DATA REDEIVED".utf8), as: UTF8.self))
-        //            }
-        //        }
-    }
     func SendClientDimension(){
         let x = TCPClient(host: AOSPServer,port: dimPort,delegate: nil)
         let screenSize     = UIScreen.main.bounds
@@ -184,14 +134,7 @@ class ViewController: UIViewController,RTPPacketDelegate,VideoDecoderDelegate {
         }
         
     }
-    func testTCPClient(){
-        let x = TCPClient(host: "localhost",port: 2399,delegate: nil)
-        for i in 1...5{
-            _ = x.send(data: Data("Test String \(i)\n".utf8))
-            sleep(3)
-        }
-        
-    }
+    
     func printConfig(){
         print("*********************************************************************")
         if self.networkModeNIO{
@@ -203,7 +146,7 @@ class ViewController: UIViewController,RTPPacketDelegate,VideoDecoderDelegate {
         print("Server IP   : \(self.AOSPServer)")
         print("Mode        : \(self.mode)")
         print("Dim Port    : \(self.dimPort)")
-        print("TCP Port    : \(self.port)")
+        print("Data Port   : \(self.port)")
         print("*********************************************************************")
         
     }
@@ -272,13 +215,8 @@ class ViewController: UIViewController,RTPPacketDelegate,VideoDecoderDelegate {
             self.receivedIndividualNalFrame(&rawPayload)
         }
         if DEBUG{
-            //            Data(rawPayload).hex()
+            Data(rawPayload).hex()
         }
-        
-        
-        
-        //        self.receivedIndividualNalFrame(&pck)    //This is where we revceive the packet and process it. Here we have to receive the packet.
-        
         
     }
     
